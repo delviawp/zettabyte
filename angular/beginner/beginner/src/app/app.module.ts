06e7eeca9b76c99215b9322ea9fd0442255646c1
delviawp <delviawp@gmail.com>
2021-02-02 09:24:54 +0800

    beginner

--- angular/beginner/beginner/src/app/app.module.ts
+++ angular/beginner/beginner/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NtcComponent } from './ntc/ntc.component';
-import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NtcComponent,
-    CartComponent
   ],
   imports: [
     BrowserModule,
